0ed95d1e23232d65fa1cb3149b7dbdc5add0f7f2
Augment Agent <emotiveimpact@gmail.com>
2025-08-29 17:52:31 -0100

    Disable Clerk provider and auth pages for deployment

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,16 +1,13 @@
 "use client"
 
-import { ClerkProvider } from '@clerk/nextjs'
 import { Toaster } from '@/components/ui/toaster'
 import { UserProvider } from '@/contexts/user-context'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <ClerkProvider>
-      <UserProvider>
-        {children}
-        <Toaster />
-      </UserProvider>
-    </ClerkProvider>
+    <UserProvider>
+      {children}
+      <Toaster />
+    </UserProvider>
   )
 }
