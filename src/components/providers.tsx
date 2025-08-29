57fca79e42b6f703755fedfc05620811ec1f885d
Augment Agent <emotiveimpact@gmail.com>
2025-08-29 17:54:26 -0100

    Disable UserProvider for deployment

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -1,13 +1,12 @@
 "use client"
 
 import { Toaster } from '@/components/ui/toaster'
-import { UserProvider } from '@/contexts/user-context'
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <UserProvider>
+    <>
       {children}
       <Toaster />
-    </UserProvider>
+    </>
   )
 }
